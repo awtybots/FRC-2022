2a357b708265537dc79ccccb7a2a79b0dcdfc69e
Alex <>
2022-02-20 21:06:53 -0600

    add tower subsystem

---
+++ src/main/java/frc/robot/subsystems/TowerSubsystem.java
@@ -0,0 +1,33 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.Tower;
+
+public class TowerSubsystem extends SubsystemBase {
+
+  private final WPI_TalonSRX upperMotor, lowerMotor;
+  private final MotorControllerGroup motors;
+
+  public TowerSubsystem() {
+    upperMotor = new WPI_TalonSRX(Tower.kUpperMotor);
+    lowerMotor = new WPI_TalonSRX(Tower.kLowerMotor);
+
+    configMotors();
+
+    motors = new MotorControllerGroup(upperMotor, lowerMotor);
+  }
+
+  private void configMotors() {
+    upperMotor.configAllSettings(Tower.motorConfig());
+    lowerMotor.configAllSettings(Tower.motorConfig());
+
+    upperMotor.setInverted(false);
+    lowerMotor.setInverted(false);
+  }
+}
