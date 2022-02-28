9a8738419a9298f6cce116d8560aca15b128f307
matt-mekha <matthew.mekha@gmail.com>
2022-02-28 17:55:33 -0600

    better conversions

--- src/main/java/frc/robot/util/math/Convert.java
+++ src/main/java/frc/robot/util/math/Convert.java
@@ -2,6 +2,8 @@ package frc.robot.util.math;
 
 public final class Convert {
 
+  // #region encoders
+
   public static enum Encoder {
     VersaPlanetaryIntegrated(1024.0),
     TalonFXIntegrated(2048.0),
@@ -14,21 +16,93 @@ public final class Convert {
     }
   }
 
-  public static double rpmToEncoderVel(double rpm, Encoder encoder) {
-    return (rpm / 60.0) * (encoder.ticksPerRevolution / 10.0);
+  public static double encoderPosToRevolutions(
+      double sensorUnits, double gearRatio, Encoder encoder) {
+    return sensorUnits / encoder.ticksPerRevolution * gearRatio;
+  }
+
+  public static double revolutionsToEncoderPos(
+      double revolutions, double gearRatio, Encoder encoder) {
+    return revolutions / gearRatio * encoder.ticksPerRevolution;
+  }
+
+  public static double encoderPosToAngle(double sensorUnits, double gearRatio, Encoder encoder) {
+    return encoderPosToRevolutions(sensorUnits, gearRatio, encoder) * 360.0;
+  }
+
+  public static double angleToEncoderPos(double angle, double gearRatio, Encoder encoder) {
+    return revolutionsToEncoderPos(angle / 360.0, gearRatio, encoder);
+  }
+
+  /** @return distance (meters) that a winch would pull or wheel would travel */
+  public static double encoderPosToDistance(
+      double sensorUnits, double gearRatio, double diameter, Encoder encoder) {
+    return encoderPosToRevolutions(sensorUnits, gearRatio, encoder) * (diameter * Math.PI);
+  }
+
+  public static double distanceToEncoderPos(
+      double distance, double gearRatio, double diameter, Encoder encoder) {
+    return revolutionsToEncoderPos(distance / (diameter * Math.PI), gearRatio, encoder);
+  }
+
+  public static double encoderVelToRevsPerSec(
+      double sensorUnits, double gearRatio, Encoder encoder) {
+    return sensorUnits * 10.0 / encoder.ticksPerRevolution * gearRatio;
+  }
+
+  public static double revsPerSecToEncoderVel(
+      double revsPerSec, double gearRatio, Encoder encoder) {
+    return revsPerSec / gearRatio * encoder.ticksPerRevolution / 10.0;
   }
 
   /** @return encoder ticks per 100ms */
-  public static double degPerSecToEncoderVel(double degPerSec, Encoder encoder) {
-    return (degPerSec / 360.0) * (encoder.ticksPerRevolution / 10.0);
+  public static double rpmToEncoderVel(double rpm, double gearRatio, Encoder encoder) {
+    return revsPerSecToEncoderVel(rpm / 60.0, gearRatio, encoder);
+  }
+
+  public static double encoderVelToRpm(
+      double sensorUnitsPer100ms, double gearRatio, Encoder encoder) {
+    return encoderVelToRevsPerSec(sensorUnitsPer100ms, gearRatio, encoder) * 60.0;
+  }
+
+  /** @return encoder ticks per 100ms */
+  public static double angularVelToEncoderVel(double degPerSec, double gearRatio, Encoder encoder) {
+    return revsPerSecToEncoderVel(degPerSec / 360.0, gearRatio, encoder);
   }
 
   /** @return (encoder ticks per 100ms) per second */
-  public static double degPerSecPerSecToEncoderAccel(double degPerSecPerSec, Encoder encoder) {
-    return (degPerSecPerSec / 360.0) * (encoder.ticksPerRevolution / 10.0);
+  public static double angularAccelToEncoderAccel(
+      double degPerSecPerSec, double gearRatio, Encoder encoder) {
+    return revsPerSecToEncoderVel(degPerSecPerSec / 360.0, gearRatio, encoder);
   }
 
-  public static double encoderVelToRpm(double sensorUnitsPer100ms, Encoder encoder) {
-    return ((sensorUnitsPer100ms * 10.0) / encoder.ticksPerRevolution) * 60.0;
+  public static double encoderVelToSpeed(
+      double sensorUnitsPer100ms, double gearRatio, double diameter, Encoder encoder) {
+    return encoderVelToRevsPerSec(sensorUnitsPer100ms, gearRatio, encoder) * (diameter * Math.PI);
   }
+
+  public static double speedToEncoderVel(
+      double speed, double gearRatio, double diameter, Encoder encoder) {
+    return revsPerSecToEncoderVel(speed / (diameter * Math.PI), gearRatio, encoder);
+  }
+
+  public static double accelToEncoderAccel(
+      double accel, double gearRatio, double diameter, Encoder encoder) {
+    return speedToEncoderVel(accel, gearRatio, diameter, encoder);
+  }
+
+  // #endregion
+
+  // #region units
+
+  public static final double inchesToMeters(double inches) {
+    return inches * 0.0254;
+  }
+
+  public static final double metersToInches(double meters) {
+    return meters / 0.0254;
+  }
+
+  // #endregion
+
 }
