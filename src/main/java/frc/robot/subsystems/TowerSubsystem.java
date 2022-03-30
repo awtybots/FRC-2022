78475a8fb1965b8db372fc417c7a509dbe64b597
Alex <>
2022-03-29 23:06:54 -0500

    move colorsensor to utils

--- src/main/java/frc/robot/subsystems/TowerSubsystem.java
+++ src/main/java/frc/robot/subsystems/TowerSubsystem.java
@@ -7,30 +7,28 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-import com.revrobotics.ColorMatch;
-import com.revrobotics.ColorMatchResult;
-import com.revrobotics.ColorSensorV3;
-import com.revrobotics.ColorSensorV3.*;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
-import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.Constants.ColorSensors;
 import frc.robot.Constants.Tower;
+import frc.util.ColorSensor;
 
 public class TowerSubsystem extends SubsystemBase {
 
   private State m_state = State.Idle;
   private boolean firing = false;
 
-  private Alliance ourAlliance;
+  private Alliance ourAlliance = Alliance.Invalid;
+  private final Color kRed = new Color(0.41, 0.41, 0.18);
+  private final Color kBlue = new Color(0.17, 0.41, 0.43);
   private final ColorSensor lowerSensor, upperSensor;
   private final WPI_TalonSRX upperMotor, lowerMotor;
 
-  private static final double kIntakingSpeedLower = 0.4;
-  private static final double kIntakingSpeedUpper = 0.3;
+  private static final double kLoadingSpeedLower = 0.4;
+  private static final double kLoadingSpeedUpper = 0.3;
 
   private static final double kReversingSpeedLower = 0.5;
   private static final double kReversingSpeedUpper = 0.4;
@@ -39,8 +37,8 @@ public class TowerSubsystem extends SubsystemBase {
   private static final double kShootingSpeedUpper = 0.75;
 
   public TowerSubsystem() {
-    upperSensor = new ColorSensor(ColorSensors.kUpperSensorPort);
-    lowerSensor = new ColorSensor(ColorSensors.kLowerSensorPort);
+    upperSensor = new ColorSensor(ColorSensors.kUpperSensorPort, kRed, kBlue, 250, 0.90);
+    lowerSensor = new ColorSensor(ColorSensors.kLowerSensorPort, kRed, kBlue, 250, 0.90);
 
     upperMotor = new WPI_TalonSRX(Tower.kUpperMotorCanId);
     lowerMotor = new WPI_TalonSRX(Tower.kLowerMotorCanId);
@@ -80,14 +78,13 @@ public class TowerSubsystem extends SubsystemBase {
       case Feeding:
         if (!firing) load();
         else {
+          if (!upperPresent) feedBoth();
           if (upperPresent) feedUpper();
-          else feedBoth();
         }
 
       case Loading:
         // TODO ensure there's no balls in the tower before we send lower one up
-        // if (!upperPresent && lowerPresent)
-        if (!upperPresent && !lowerPresent) loadBoth();
+        if (!upperPresent) loadBoth();
         if (upperPresent && !lowerPresent) loadLower();
         if (upperPresent && lowerPresent) stop();
 
@@ -140,12 +137,12 @@ public class TowerSubsystem extends SubsystemBase {
   }
 
   private void loadBoth() {
-    lowerMotor.set(ControlMode.PercentOutput, kIntakingSpeedLower);
-    upperMotor.set(ControlMode.PercentOutput, kIntakingSpeedUpper);
+    lowerMotor.set(ControlMode.PercentOutput, kLoadingSpeedLower);
+    upperMotor.set(ControlMode.PercentOutput, kLoadingSpeedUpper);
   }
 
   private void loadLower() {
-    lowerMotor.set(ControlMode.PercentOutput, kIntakingSpeedLower);
+    lowerMotor.set(ControlMode.PercentOutput, kLoadingSpeedLower);
     upperMotor.set(ControlMode.PercentOutput, 0.0);
   }
 
@@ -162,7 +159,9 @@ public class TowerSubsystem extends SubsystemBase {
   /// -------- Color Sensors -------- ///
   /** Note: will return false if no ball present */
   public boolean upperBallOurs() {
-    return upperBallPresent() && (upperSensor.getBallAlliance() == ourAlliance);
+    final var ballAlliance = upperSensor.getBallAlliance();
+    return upperBallPresent() && (ballAlliance == ourAlliance)
+        || (ballAlliance == Alliance.Invalid);
   }
 
   private boolean upperBallPresent() {
@@ -175,64 +174,5 @@ public class TowerSubsystem extends SubsystemBase {
 
   public void setAlliance(Alliance ours) {
     this.ourAlliance = ours;
-  }
-
-  private class ColorSensor {
-
-    private final int minimumDistance = 250;
-    private final double minColorConfidence = 0.90;
-
-    // Tune these at each field if you want to know the color of the balls
-    private final Color Red = new Color(0.41, 0.41, 0.18);
-    private final Color Blue = new Color(0.17, 0.41, 0.43);
-
-    private final ColorSensorV3 sensor;
-    private final ColorMatch colorMatch = new ColorMatch();
-
-    public ColorSensor(I2C.Port port) {
-      sensor = new ColorSensorV3(port);
-      sensor.configureColorSensor(
-          ColorSensorResolution.kColorSensorRes17bit,
-          ColorSensorMeasurementRate.kColorRate25ms,
-          GainFactor.kGain3x);
-      sensor.configureProximitySensor(
-          ProximitySensorResolution.kProxRes11bit, ProximitySensorMeasurementRate.kProxRate6ms);
-
-      sensor.configureProximitySensor(
-          ProximitySensorResolution.kProxRes11bit, ProximitySensorMeasurementRate.kProxRate6ms);
-
-      sensor.configureColorSensor(
-          ColorSensorResolution.kColorSensorRes17bit,
-          ColorSensorMeasurementRate.kColorRate50ms,
-          GainFactor.kGain3x);
-
-      colorMatch.addColorMatch(Red);
-      colorMatch.addColorMatch(Blue);
-      colorMatch.setConfidenceThreshold(minColorConfidence);
-    }
-
-    public boolean ballPresent() {
-      return sensor.getProximity() > minimumDistance;
-    }
-
-    public Alliance getBallAlliance() {
-      Color detectedColor = sensor.getColor();
-      ColorMatchResult match = colorMatch.matchColor(detectedColor);
-
-      if (match != null) {
-        if (match.color == Red) return Alliance.Red;
-        if (match.color == Blue) return Alliance.Blue;
-      }
-
-      return Alliance.Invalid;
-    }
-
-    public String rawColor() {
-      return rgbToString(sensor.getColor());
-    }
-
-    private String rgbToString(Color c) {
-      return String.format("RGB(%.2f, %.2f, %.2f)", c.red, c.green, c.blue);
-    }
   }
 }
