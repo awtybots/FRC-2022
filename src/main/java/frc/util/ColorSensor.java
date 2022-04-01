c48d52e208e0c6aa9e19657318aa6a964d9f2976
matt-mekha <51768075+matt-mekha@users.noreply.github.com>
2022-04-01 17:37:01 -0500

    add colorsensor to util

---
+++ src/main/java/frc/util/ColorSensor.java
@@ -0,0 +1,72 @@
+package frc.util;
+
+import com.revrobotics.ColorMatch;
+import com.revrobotics.ColorMatchResult;
+import com.revrobotics.ColorSensorV3;
+import com.revrobotics.ColorSensorV3.*;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.util.Color;
+
+public class ColorSensor {
+
+  private final int minimumDistance;
+  private final double minColorConfidence;
+
+  private final Color Red, Blue;
+
+  private final ColorSensorV3 sensor;
+  private final ColorMatch colorMatch = new ColorMatch();
+
+  public ColorSensor(I2C.Port port, Color red, Color blue, int minDistance, double minConfidence) {
+    this.minimumDistance = minDistance;
+    this.minColorConfidence = minConfidence;
+    this.Red = red;
+    this.Blue = blue;
+
+    colorMatch.addColorMatch(red);
+    colorMatch.addColorMatch(blue);
+    colorMatch.setConfidenceThreshold(minColorConfidence);
+
+    sensor = new ColorSensorV3(port);
+
+    sensor.configureColorSensor(
+        ColorSensorResolution.kColorSensorRes17bit,
+        ColorSensorMeasurementRate.kColorRate25ms,
+        GainFactor.kGain3x);
+    sensor.configureProximitySensor(
+        ProximitySensorResolution.kProxRes11bit, ProximitySensorMeasurementRate.kProxRate6ms);
+
+    sensor.configureProximitySensor(
+        ProximitySensorResolution.kProxRes11bit, ProximitySensorMeasurementRate.kProxRate6ms);
+
+    sensor.configureColorSensor(
+        ColorSensorResolution.kColorSensorRes17bit,
+        ColorSensorMeasurementRate.kColorRate50ms,
+        GainFactor.kGain3x);
+  }
+
+  public boolean ballPresent() {
+    return sensor.getProximity() > minimumDistance;
+  }
+
+  public Alliance getBallAlliance() {
+    Color detectedColor = sensor.getColor();
+    ColorMatchResult match = colorMatch.matchColor(detectedColor);
+
+    if (match != null) {
+      if (match.color == Red) return Alliance.Red;
+      if (match.color == Blue) return Alliance.Blue;
+    }
+
+    return Alliance.Invalid;
+  }
+
+  public String rawColor() {
+    return rgbToString(sensor.getColor());
+  }
+
+  private String rgbToString(Color c) {
+    return String.format("RGB(%.2f, %.2f, %.2f)", c.red, c.green, c.blue);
+  }
+}
