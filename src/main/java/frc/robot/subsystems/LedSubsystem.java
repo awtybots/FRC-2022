1f6776d5a196c1e3ab2d90e56d9ad5a3b9b51fe5
matt-mekha <51768075+matt-mekha@users.noreply.github.com>
2022-05-20 16:23:09 -0500

    fix alex bad code

--- src/main/java/frc/robot/subsystems/LedSubsystem.java
+++ src/main/java/frc/robot/subsystems/LedSubsystem.java
@@ -2,7 +2,6 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.PowerDistribution;
 import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class LedSubsystem extends SubsystemBase {
@@ -23,23 +22,12 @@ public class LedSubsystem extends SubsystemBase {
 
   public LedSubsystem() {
     pdh = new PowerDistribution();
-    SmartDashboard.putBoolean("LED POWERRRRR", false);
-    SmartDashboard.putBoolean("blinky blinky", false);
 
     turnOff();
   }
 
   @Override
   public void periodic() {
-    final boolean l = SmartDashboard.getBoolean("LED POWERRRRR", false);
-    final boolean b = SmartDashboard.getBoolean("blinky blinky", false);
-    if (b) {
-      blink();
-    } else if (l) {
-      turnOn();
-    } else {
-      turnOff();
-    }
     switch (state) {
       case kBlinkingOn:
         if (timer.get() > kBlinkPeriod) {
